4f103790517a6c8522c5e77ff3f937fe1e7b7001
Shmuli Feld <shmuli@Shmulis-MBP.quinnipiac.edu>
2024-02-14 17:12:56 -0500

    initial commit

---
+++ mafia/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  mafia
+//
+//  Created by Shmuli Feld on 2/6/24.
+//
+
+import SwiftUI
+import SwiftData
+
+enum helloWorld: String, CaseIterable {
+   case Hello_World, שלום_עולם, Hola_Mundo
+}
+
+struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+    
+    @State var selection: helloWorld = .Hello_World
+
+    var body: some View {
+        NavigationView {
+            VStack{
+                Text(selection.rawValue)
+                    .font(.system(size: 125))
+                Picker("Select Laungauge", selection: $selection) {
+                    ForEach(helloWorld.allCases, id: \.self){
+                        helloWorld in Text(helloWorld.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+        }
+    }
+
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(timestamp: Date())
+            modelContext.insert(newItem)
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(items[index])
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+        .modelContainer(for: Item.self, inMemory: true)
+}
---
+++ mafia/mafiaApp.swift
@@ -0,0 +1,32 @@
+//
+//  mafiaApp.swift
+//  mafia
+//
+//  Created by Shmuli Feld on 2/6/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct mafiaApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
